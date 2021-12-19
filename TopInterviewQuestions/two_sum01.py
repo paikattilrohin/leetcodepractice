c5e4c353ac07fb48fdebc163da55e3f657536f4f
Rohin Paikattil <paikattilrohin@gmail.com>
2021-12-19 02:07:06 -0500

    01

---
+++ TopInterviewQuestions/two_sum01.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i, num in enumerate(nums):
+            if target - num in dict:
+                return [dict[target- num], i]
+            else:
+                dict[num] = i
+        return [-1,-1]
